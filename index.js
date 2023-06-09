c7129206c8c8bfde6a34435a32c724ca359525c4
satish chakma <satishchakma.gd@gmail.com>
2023-06-09 21:28:42 +0600

    verify jwt and axios

--- index.js
+++ index.js
@@ -9,8 +9,29 @@ const jwt = require("jsonwebtoken");
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Summer server is running!");
 });
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -63,6 +84,13 @@ async function run() {
       const cursor = userCollection.find({});
       const result = await cursor.toArray();
       res.send(result);
+    });
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send(token);
     });
 
     app.patch("/users/admin/:id", async (req, res) => {
