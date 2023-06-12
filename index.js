bd11b2de46981bdfd14b660627009b96493088d3
satish chakma <satishchakma.gd@gmail.com>
2023-06-12 15:39:14 +0600

    added isadmin and is instructor codes

--- index.js
+++ index.js
@@ -139,16 +139,16 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+    app.get("/users/instructors/:email", async (req, res) => {
       const email = req.params.email;
 
-      if (req.decoded.email !== email) {
-        res.send({ admin: false });
-      }
+      // if (req.decoded.email !== email) {
+      //   res.send({ instructor: false });
+      // }
 
       const query = { email: email };
       const user = await userCollection.findOne(query);
-      const result = { admin: user?.role === "instructor" };
+      const result = { instructor: user?.role === "instructor" };
       res.send(result);
     });
     app.patch("/users/instructor/:id", async (req, res) => {
