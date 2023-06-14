bb690ef68facb75f8322df84ae971fc236f54839
satish chakma <satishchakma.gd@gmail.com>
2023-06-14 16:55:15 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
 // middleswares
-app.use(cors());
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
